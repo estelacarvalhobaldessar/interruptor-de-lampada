41d9e47073acd73e3a2c9fe1599c5315d327a2ff
Estela de C. Baldessar <ecbaldessar@gmail.com>
2025-03-27 12:13:07 -0300

    teste1

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnLigar = document.getElementById('btnLigar');
+btnLigar.addEventListener("click", function () {
+    let lampada = document.getElementById('lampada');
+    lampada.src = "./img/lampada acesa.png";
+})
